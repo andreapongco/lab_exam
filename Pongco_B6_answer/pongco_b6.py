e874d5600af9067617c7939baa0b30fba5fa2884
VON_ARELANO <vbarellano@up.edu.ph>
2023-12-04 12:49:16 +0800

    Done :>

--- Pongco_B6_answer/pongco_b6.py
+++ Pongco_B6_answer/pongco_b6.py
@@ -22,9 +22,9 @@ table = readCSVFile()
 for i in range(0, 61):
     table[i] = table[i].split(",")
     if i == 0:
-        table[i].append("HRID\n")
+        table[i].append("HRID,\n")
     else:
-        table[i].append(f"{table[i][1]}-{table[i][2]}-{table[i][3]}-{table[i][6]}\n")
+        table[i].append(f"{table[i][1]}-{table[i][2]}-{table[i][3]}-{table[i][6]},\n")
 
 write_in_file = []
 for i in range(0, 61):
